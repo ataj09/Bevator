ed114628d934ad0ada8b6e5781c78e756ac2fba2
Ataj09 <michal@bryl.net>
2023-04-17 18:17:21 +0200

    changed json struct

--- bin/NLP.py
+++ bin/NLP.py
@@ -1,11 +1,20 @@
 import spacy
 import json
+import os
 
 class NLP:
     def __init__(self):
         import spacy
         self.nlp = spacy.load('en_core_web_sm')
         self.text = ""
+        self.path = os.path.join(os.getcwd(),"../data")
+        self.phrases = {}
+
+    def load_dict(self):
+
+        for i in os.listdir(self.path):
+            with open(i) as jsonfile:
+                temp = json.load(jsonfile)
 
     def getKeys(self, text):
         self.text = text.lower()
--- bin/main.py
+++ bin/main.py
@@ -1,2 +1,10 @@
 import threading
 import Speech2text
+import NLP
+
+def main():
+    nlp = NLP.NLP()
+    s2t = Speech2text.Speech2text(nlp)
+
+if __name__ == "__main__":
+    main()
